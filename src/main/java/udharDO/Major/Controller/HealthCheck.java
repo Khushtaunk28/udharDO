231e32ab43e13369d4d021194bf3247b8bc5b493
Khushtaunk28 <Khush.taunk@gmail.com>
2024-10-13 00:19:46 +0530

    connected end points and created the health checkup

--- src/main/java/udharDO/Major/Controller/HealthCheck.java
+++ src/main/java/udharDO/Major/Controller/HealthCheck.java
@@ -1,11 +1,13 @@
 package udharDO.Major.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/health-check")
 public class HealthCheck {
+    @GetMapping
     private String check(){
         return "OK,udharDo is working Great";
     }
--- src/main/java/udharDO/Major/Controller/ShopKeeperController.java
+++ src/main/java/udharDO/Major/Controller/ShopKeeperController.java
@@ -10,12 +10,14 @@ public class ShopKeeperController {
     //POST /shopkeeper/{shopkeeperId}/loan: Create a new loan for a customer.
     //GET /shopkeeper/{shopkeeperId}/loans: View all loans issued.
     //GET /shopkeeper/{shopkeeperId}/earnings: View earnings from interest.
+    @GetMapping("health-check")
+    private String check(){
+        return "shopkeeper is good";
+    }
     @GetMapping("id/{id}")
     public String viewLoans(@PathVariable String id) {
         return "healthy";
     }
-
-
     @PostMapping("/create-loan")
     public String createLoan() {
         return "success";
